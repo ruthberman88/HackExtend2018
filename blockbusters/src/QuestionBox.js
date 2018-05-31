6472fc63f338e2b9a9b1fb564f9df1e2a77fd2be
ruth <ruth.berman88@gmail.com>
2018-05-31 17:24:02 +0300

    adding questionBox file

---
+++ blockbusters/src/QuestionBox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './QuestionBox.css';
+
+class QuestionBox extends React.Component {
+
+  render() {
+    return (
+      <div className="QuestionBox">
+          This is the QuestionBox page
+      </div>
+    );
+  }
+}
+
+export default QuestionBox;
