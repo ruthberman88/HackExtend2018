e66bbcb6ce1d169c8cee5baf83c46a7dce86fc3f
ruth <ruth.berman88@gmail.com>
2018-05-31 12:32:34 +0300

    change dir

---
+++ frontend/blockbusters/src/LandingPage.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './LandingPage.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="LandingPage">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to BlockBusters!</h1>
+        </header>
+        <p className="App-intro">
+          This is the link you need
+        </p>
+      </div>
+    );
+  }
+}
+
+class TeamBox extends React.Component {
+  
+}
+/*export default App;
