c3249271765dd89d8f92f59282c72f803af995ca
ruth <ruth.berman88@gmail.com>
2018-05-31 19:46:08 +0300

    clicking on hex navigates to question

--- blockbusters/src/GamePage.js
+++ blockbusters/src/GamePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './GamePage.css';
+import QuestionBox from './QuestionBox.js';
 
 class GamePage extends React.Component {
 
@@ -20,18 +21,34 @@ class Board extends React.Component {
       console.log("in Board")
         return (
           <div>
-          <Hexagon color="hexagon-grey"/>
-          <Hexagon color="hexagon-grey"/>
+          <Hexagon color="hexagon-grey" number="1"/>
+          <Hexagon color="hexagon-blue" number="2"/>
           </div>
         );
   }
 }
 
 class Hexagon extends React.Component {
- render () {
-   console.log("in Hexagon")
-   return <div className={this.props.color}><span></span>  </div>;
- }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
+
+  handleClick() {
+    this.setState({value: <QuestionBox />})
+  }
+
+  render() {
+    return (
+      <div className={this.props.color} onClick={ () => this.handleClick()}> <span></span>
+        {this.props.number}
+        {this.state.value}
+      </div>
+    );
+  }
 }
 
 export default GamePage;
