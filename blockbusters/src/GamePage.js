3afdb029a8c0ca378789b01e47d69ff8df406b19
ruth <ruth.berman88@gmail.com>
2018-05-31 16:44:37 +0300

    adding usercounter and game page

---
+++ blockbusters/src/GamePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './GamePage.css';
+
+class GamePage extends React.Component {
+
+  render() {
+    return (
+      <div className="GamePage">
+        <p className="App-intro">
+          This is the game page
+        </p>
+      </div>
+    );
+  }
+}
+
+export default GamePage;
