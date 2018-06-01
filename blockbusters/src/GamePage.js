16cb3d9ed99dc33cd813a2f6516eb2c632e86167
Ori Moisis <omoisis@magicleap.com>
2018-06-01 09:52:12 +0300

    small size changes

--- blockbusters/src/GamePage.js
+++ blockbusters/src/GamePage.js
@@ -57,32 +57,38 @@ class Board extends React.Component {
                 team={this.state.currTeam}
                 onClick={(winningTeam) => this.handleReturnFromQuestion(winningTeam)}/>
     }
+    var nextTeamName = ["red", "blue"][this.state.currTeam];
     return (
-      <div className="frame">
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-          {this.renderSquare(3)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(8)}
-          {this.renderSquare(9)}
-          {this.renderSquare(10)}
-          {this.renderSquare(11)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(12)}
-          {this.renderSquare(13)}
-          {this.renderSquare(14)}
-          {this.renderSquare(15)}
-        </div>
+      <div>
+          <div className="LandingPage-title" style={{ backgroundColor: nextTeamName }}>
+            next team is {nextTeamName}
+          </div>
+          <div className="frame">
+            <div className="board-row">
+              {this.renderSquare(0)}
+              {this.renderSquare(1)}
+              {this.renderSquare(2)}
+              {this.renderSquare(3)}
+            </div>
+            <div className="board-row">
+              {this.renderSquare(4)}
+              {this.renderSquare(5)}
+              {this.renderSquare(6)}
+              {this.renderSquare(7)}
+            </div>
+            <div className="board-row">
+              {this.renderSquare(8)}
+              {this.renderSquare(9)}
+              {this.renderSquare(10)}
+              {this.renderSquare(11)}
+            </div>
+            <div className="board-row">
+              {this.renderSquare(12)}
+              {this.renderSquare(13)}
+              {this.renderSquare(14)}
+              {this.renderSquare(15)}
+            </div>
+          </div>
       </div>
     );
   }
